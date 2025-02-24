366e21f6e59bb3a595e6db180b3b3a2ceae88325
[Saqib-Sheikh] <[saqisheikh86@gmail.com]>
2025-02-24 16:56:39 +0500

    day 1

--- day1.py
+++ day1.py
@@ -1,3 +1,9 @@
+
 name_1 = "Saqib"
 
-print(f"Hello,I'm {name_1} Python Learners! 🚀 Today is the first step in our coding journey.")
+print(f"Hello,I'm {name_1} Python Learners! Today is the first step in our coding journey.")
+
+
+# modify code
+name= input("Enter your name? ")
+print(f"""Hello,I'm {name_1} Python Learners! 🚀 Today is the first step in our coding journey. let's grow together Mr. {name}""")
