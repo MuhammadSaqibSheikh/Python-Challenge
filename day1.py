d3d0a10d2e9d6ecbfff68b720b28cd84833ed86d
[Saqib-Sheikh] <[saqisheikh86@gmail.com]>
2025-02-25 17:40:43 +0500

    day2 challenge

--- day1.py
+++ day1.py
@@ -7,3 +7,5 @@ print(f"Hello,I'm {name_1} Python Learners! Today is the first step in our codin
 # modify code
 name= input("Enter your name? ")
 print(f"""Hello,I'm {name_1} Python Learners! 🚀 Today is the first step in our coding journey. let's grow together Mr. {name}""")
+
+
---
+++ day2.py
@@ -0,0 +1,7 @@
+
+
+sentance = "python is amazing!"
+word = sentance.split()
+word_count = len(word)
+
+print(f"total letter count: {word_count}")
