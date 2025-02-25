11e9bbab41ce483e3e67634f47eadf177b2588a7
[Saqib-Sheikh] <[saqisheikh86@gmail.com]>
2025-02-25 17:54:59 +0500

    bouns  challenge

--- day2.py
+++ day2.py
@@ -2,6 +2,10 @@
 
 sentance = "python is amazing!"
 word = sentance.split()
+reversed = word [::-1]
+joined = ' '.join(reversed)
+print(joined)
+
 word_count = len(word)
 
 print(f"total words count: {word_count}")
